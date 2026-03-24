cdd64b811bbc994998b4969371ab5e402adadec5
Ersi Isufi <ersi.isufi@hotmail.com>
2026-03-24 18:10:53 +0300

    Rearranged prediction pipeline

---
+++ README.md
@@ -0,0 +1,27 @@
+# Student Performance Prediction
+
+## Problem
+Predict student exam performance based on study habits and past data.
+
+## Approach
+- Data cleaning
+- Linear regression model
+- Evaluation using MSE and R²
+
+## Results
+- MSE: 28
+- R²: 0.88
+
+## How to Run
+
+### Train model
+python train_pipeline.py
+
+### Predict
+python predict_pipeline.py
+
+## Project Structure
+- data/
+- src/
+- train_pipeline.py
+- predict_pipeline.py
--- predict_pipeline.py
+++ predict_pipeline.py
@@ -5,14 +5,22 @@ def main():
 
     model = load_model()
 
-    new_Data = [[0,1,3,4,6,7,8,]]
+    print("Enter student Data: ")
+
+    id = float(input(""))
+    gender = float(input("Gender 1,0: "))
+    lunch = float(input("Lunch 1,0: "))
+    reading = float(input("Reading: "))
+    writing = float(input("Writing: "))
+    parental = float(input("Parental level of educ 1-3: "))
+    prep = float(input("Test preparation 1,0; "))
+
+    new_Data = [[id, gender, lunch, reading, writing, parental, prep]]
 
     predicition = model.predict(new_Data)
-
     print("Prediction: ", predicition)
 
 
-    print("Prediction Pipeline finished")
 
 if __name__ == "__main__":
     main()
